810b40ee62bd8aa279571fef0b92384d33dc79fc
nujinging <nujinging@naver.com>
2023-12-09 19:40:03 +0900

    마이페이지 작업중

--- front/src/page/Profile.js
+++ front/src/page/Profile.js
@@ -1,5 +1,8 @@
+import './../scss/profile.scss'
 import HomeFooter from "../components/HomeFooter";
 import Title from "../components/Title";
+import {Link} from "react-router-dom";
+import React from "react";
 
 export default function Profile() {
   return (
@@ -8,7 +11,28 @@ export default function Profile() {
       <Title title="마이페이지"></Title>
 
       <div className="container">
+        <div className="user_info">
+          <h3 className="name">누징</h3>
+          <p className="visit_count">🏡 연속 방문 최대 <strong>12</strong>번을 달성했어요!</p>
+        </div>
+        <div className="user_con">
+          <div className="user_tit">
+            <h4>
+              등록한 단어
+              <span className="count">3</span>
+            </h4>
+            <Link to="/word" className="item">더보기</Link>
+          </div>
 
+          <ul className="word_list">
+            <li>
+              <Link to="/word/1" className="item">개개개</Link>
+            </li>
+            <li>
+              <Link to="" className="item">거거거</Link>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <HomeFooter></HomeFooter>
