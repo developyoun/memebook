9c905ee6f9fc58394ce4098e498318ab39c4bd66
pyoun <pyoun820@naver.com>
2024-03-21 21:45:12 +0900

    [feat][back] cors 이슈 해결

--- back/src/main/java/meme/book/back/config/SecurityConfig.java
+++ back/src/main/java/meme/book/back/config/SecurityConfig.java
@@ -1,15 +1,6 @@
 package meme.book.back.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
 @RequiredArgsConstructor
