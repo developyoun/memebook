591b3b464c2b3b0cee1372d43a803edea05ddec8
nujinging <nujinging@naver.com>
2024-03-26 14:07:16 +0900

    [design] 55th 단어 타이틀 제거

--- front/src/page/Library.js
+++ front/src/page/Library.js
@@ -45,51 +45,6 @@ export default function Word() {
       <Title title="사전"></Title>
 
       <div className="container">
-        <ul className="consonant_list">
-          <li className="list">
-            <a href="" className="link active">ㄱ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㄴ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㄷ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㄹ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅁ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅂ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅅ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅇ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅈ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅊ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅋ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅌ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅍ</a>
-          </li>
-          <li className="list">
-            <a href="" className="link">ㅎ</a>
-          </li>
-        </ul>
-
 
         <ul className="word_wrap">
           <li className="word_box">
