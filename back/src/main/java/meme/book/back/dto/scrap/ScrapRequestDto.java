9fa5bbddb70dc36e01b780dce49ea1b418357598
nujinging <nujinging@naver.com>
2024-04-21 15:15:43 +0900

    [fix] 스크랩 수정

---
+++ back/src/main/java/meme/book/back/dto/scrap/ScrapRequestDto.java
@@ -0,0 +1,13 @@
+package meme.book.back.dto.scrap;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class ScrapRequestDto {
+
+    private Long wordIdx;
+
+    private Long memberIdx;
+}
