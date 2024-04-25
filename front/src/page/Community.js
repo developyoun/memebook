e2af3d3bcdf283c89d756d48c0b092be02baa055
nujinging <nujinging@naver.com>
2024-04-25 17:30:11 +0900

    [Design] 100th 커뮤니티 ui 개선중

--- front/src/page/Community.js
+++ front/src/page/Community.js
@@ -19,43 +19,53 @@ export default function Community() {
         </SwiperSlide>
       </Swiper>
       
-      <div className="community_list">
-        <ul className="list">
-          <li>
-            <Link to="" className="content_link">
-              <h3 className="tit">요즘 잠이 안와요</h3>
-              <p className="txt">왜 안오는지 누가 알려주실래요 괴롭네요</p>
-            </Link>
-            <div className="community_reaction">
-              <button type="button" className="content_like_btn">
-                <span className="blind">좋아요</span>
-              </button>
-              <button type="button" className="comment_btn">
-                <span>댓글</span>
-              </button>
-              <button type="button" className="view_btn">
-                <span>조회수</span>
-              </button>
-            </div>
-          </li>
-          <li>
-            <Link to="" className="content_link">
-              <h3 className="tit">요즘 잠이 안와요</h3>
-              <p className="txt">왜 안오는지 누가 알려주실래요 괴롭네요</p>
-            </Link>
-            <div className="community_reaction">
-              <button type="button" className="content_like_btn">
-                <span className="blind">좋아요</span>
-              </button>
-              <button type="button" className="comment_btn">
-                <span>댓글</span>
-              </button>
-              <button type="button" className="view_btn">
-                <span>조회수</span>
-              </button>
-            </div>
-          </li>
-        </ul>
+      <div className="post_box">
+        <div className="post_list">
+          <ul className="list">
+            <li>
+              <Link to="/postDetail" className="post_link">
+                <h3 className="tit">요즘 잠이 안와요</h3>
+                <p className="txt">왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요</p>
+              </Link>
+              <button type="button" className="post_more_btn">더보기</button>
+              <div className="post_reaction">
+                <button type="button" className="post_like_btn">
+                  <span className="blind">좋아요</span>
+                </button>
+                <button type="button" className="comments_btn">
+                  <span>댓글</span>
+                </button>
+                <button type="button" className="view_btn">
+                  <span>조회수</span>
+                </button>
+              </div>
+            </li>
+
+            <li>
+              <Link to="/postDetail" className="post_link">
+                <h3 className="tit">요즘 잠이 안와요</h3>
+                <p className="txt">왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요왜 안오는지 누가 알려주실래요 괴롭네요</p>
+              </Link>
+              <button type="button" className="post_more_btn">더보기</button>
+              <div className="post_reaction">
+                <button type="button" className="post_like_btn">
+                  <span className="blind">좋아요</span>
+                </button>
+                <button type="button" className="comments_btn">
+                  <span>댓글</span>
+                </button>
+                <button type="button" className="view_btn">
+                  <span>조회수</span>
+                </button>
+              </div>
+              <Link to="/postDetail" className="post_contents">
+                <span className="nickname">변태호</span>
+                <p className="comments">수면제를 드세요</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   );
