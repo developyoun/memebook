663815fd5ea3aaaef20cd7a7e0548fc1b043457f
nujinging <nujinging@naver.com>
2024-05-07 17:09:04 +0900

    [Design] 116th 메인화면 디자인

--- front/src/page/Main.js
+++ front/src/page/Main.js
@@ -61,7 +61,7 @@ export default function Main() {
         )
       }
 
-      <div className="main">
+      <div className="main_wrap">
 
         <div className="container">
          <div className="main_top">
