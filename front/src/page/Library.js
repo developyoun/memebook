8feda0e85fea59afebda47dfa897add9bf37e4a0
nujinging <nujinging@naver.com>
2024-05-07 16:48:02 +0900

    [Design] 114th 사전 디자인

--- front/src/page/Library.js
+++ front/src/page/Library.js
@@ -7,6 +7,7 @@ import 'swiper/css';
 import {useDispatch, useSelector} from "react-redux";
 import {wordListData, wordSortData} from "../util/action/wordAction";
 import {commonEvent} from "../js/commonEvent"
+import Header from "../components/Header";
 
 export default function Word() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ export default function Word() {
     async function libraryList() {
       try {
         await dispatch(wordListData('ALL', pageNumber));
+        console.log(wordList)
       } catch (error) {
         console.log(error)
       }
@@ -80,75 +82,77 @@ export default function Word() {
   }
 
   return (
-    <div className="library_wrap">
-      <header className="header">
-        <Link to="" className="logo">memeBook</Link>
-        <input type="text" className="search_input" placeholder="단어를 검색해보세요"/>
-      </header>
-      <div className="container">
-        <div className="library_top">
-          <h2 className="tit">&#128214; 사전</h2>
-          <div className="box_btn">
-            <span className="txt">사전에 없는 단어가 있나요?<br/>지금 등록해보세요 &#128073;</span>
-            <Link to="/wordAdd" className="word_add_btn">단어 등록하기 </Link>
-          </div>
-        </div>
-
-        <div className="library_box">
-
-
-          { libraryData === undefined && loadingState && (
-            <div>
-              로딩중
+    <>
+      <Header></Header>
+      <div className="library_wrap">
+        <div className="container">
+          <div className="library_top">
+            <h2 className="tit">&#128214; 사전</h2>
+            <div className="box_btn">
+              <span className="txt">사전에 없는 단어가 있나요?<br/>지금 등록해보세요 &#128073;</span>
+              <Link to="/wordAdd" className="word_add_btn">단어 등록하기 </Link>
             </div>
-          )
-          }
+          </div>
+
+          <div className="library_box">
+
+
+            { libraryData === undefined && loadingState && (
+              <div>
+                로딩중
+              </div>
+            )
+            }
+
+            {
+              libraryData !== undefined && (
+                <>
+                  <Swiper
+                    slidesPerView='auto'
+                    className="tab_box"
+                  >
+                    <SwiperSlide className={`tab_item ${libraryTab === 'ALL' ? 'active' : ''}`}>
+                      <button type="button" className="item" onClick={() => wordSortBtn('ALL')}>&#128218; 전체</button>
+                    </SwiperSlide>
+                    <SwiperSlide className={`tab_item ${libraryTab === 'LIKE' ? 'active' : ''}`}>
+                      <button type="button" className="item" onClick={() => wordSortBtn('LIKE')}>&#128077; 좋아요순</button>
+                    </SwiperSlide>
+                    <SwiperSlide className={`tab_item ${libraryTab === 'DISLIKE' ? 'active' : ''}`}>
+                      <button type="button" className="item" onClick={() => wordSortBtn('DISLIKE')}>&#128078; 싫어요순</button>
+                    </SwiperSlide>
+                    <SwiperSlide className={`tab_item ${libraryTab === 'LATEST' ? 'active' : ''}`}>
+                      <button type="button" className="item" onClick={() => wordSortBtn('LATEST')}>&#127775; 최신순</button>
+                    </SwiperSlide>
+                  </Swiper>
+                  <ul className="list_box">
+                    {
+                      libraryData?.map((item, idx) => {
+                        return (
+                          <li className="list_item">
+                            <Link to={`/word/${item.wordIdx}`} className="link" key={idx}>{item.wordName}</Link>
+                            <span className="content_count">
+                            3
+                          </span>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </>
+              )
+            }
+          </div>
 
           {
-            libraryData !== undefined && (
-              <>
-                <Swiper
-                  slidesPerView='auto'
-                  className="library_tab"
-                >
-                  <SwiperSlide className={`tab_item ${libraryTab === 'ALL' ? 'active' : ''}`}>
-                    <button type="button" className="item" onClick={() => wordSortBtn('ALL')}>전체</button>
-                  </SwiperSlide>
-                  <SwiperSlide className={`tab_item ${libraryTab === 'LIKE' ? 'active' : ''}`}>
-                    <button type="button" className="item" onClick={() => wordSortBtn('LIKE')}>좋아요순</button>
-                  </SwiperSlide>
-                  <SwiperSlide className={`tab_item ${libraryTab === 'DISLIKE' ? 'active' : ''}`}>
-                    <button type="button" className="item" onClick={() => wordSortBtn('DISLIKE')}>싫어요순</button>
-                  </SwiperSlide>
-                  <SwiperSlide className={`tab_item ${libraryTab === 'LATEST' ? 'active' : ''}`}>
-                    <button type="button" className="item" onClick={() => wordSortBtn('LATEST')}>최신순</button>
-                  </SwiperSlide>
-                </Swiper>
-                <ul className="list_box">
-                  {
-                    libraryData?.map((item, idx) => {
-                      return (
-                        <li className="list_item">
-                          <Link to={`/word/${item.wordIdx}`} className="link" key={idx}>{item.wordName}</Link>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
-              </>
+            window.scrollY > 20 && (
+              <button type="button" className="btn_top" onClick={commonEvent}>
+                <span className="blind">올리기</span>
+              </button>
             )
           }
         </div>
-
-        {
-          window.scrollY > 20 && (
-            <button type="button" className="btn_top" onClick={commonEvent}>
-              <span className="blind">올리기</span>
-            </button>
-          )
-        }
       </div>
-    </div>
+    </>
   );
 }
 
