c093417c9608d928c77f8bc27c0dbe81047a35a8
pyoun <pyoun820@naver.com>
2024-05-29 00:06:50 +0900

    [conf] back 빌드 수정

--- back/src/main/java/meme/book/back/controller/IndexController.java
+++ back/src/main/java/meme/book/back/controller/IndexController.java
@@ -1,22 +1,16 @@
 package meme.book.back.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
-public class IndexController {
+public class IndexController implements ErrorController {
 
-    @ResponseBody
-    @GetMapping("/")
-    public String test() {
-        return "SUCCESS";
-    }
-
-    @GetMapping("home")
-    public String home() {
-        return "home";
+    @GetMapping({"/", "/error"})
+    public String handleError() {
+        return "index.html";
     }
 }
--- back/src/main/resources/static/utils.js
+++
@@ -1,9 +0,0 @@
-const getElementById = (id) => {
-  return document.getElementById(id);
-}
-
-const createElement = (type, innerText) => {
-  const el = document.createElement(type);
-  innerText && (el.innerText = innerText);
-  return el;
-}
