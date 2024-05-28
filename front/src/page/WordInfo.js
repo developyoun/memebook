27cb32eec197ac7eebe090d333194ba23b1d5837
nujinging <nujinging@naver.com>
2024-05-28 12:56:58 +0900

    [Design] 169th 단어장 레이아웃 변경

--- front/src/page/WordInfo.js
+++ front/src/page/WordInfo.js
@@ -13,7 +13,7 @@ import BtnBack from "../components/BtnBack";
 export default function WordInfo() {
   let {id} = useParams();
   const dispatch = useDispatch();
-  const [memberIdx, setMemberIdx] = useState(321);
+  const [memberIdx, setMemberIdx] = useState(123);
   // 단어 데이터
   const [wordData, setWordData] = useState([]);
   const [wordListData, setWordListData] = useState([]);
