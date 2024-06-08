eed1d1eca93346049b15e0597619a0cffb0ec4ae
pyoun <pyoun820@naver.com>
2024-06-08 13:35:15 +0900

    [fix] 스웨거 문서 수정

--- back/src/main/java/meme/book/back/config/SwaggerConfig.java
+++ back/src/main/java/meme/book/back/config/SwaggerConfig.java
@@ -3,17 +3,31 @@ package meme.book.back.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+@Slf4j
 @Configuration
 public class SwaggerConfig {
 
+    @Value(value = "${springdoc.domain.url}")
+    private String domain;
+
     @Bean
     public OpenAPI openAPI() {
+
+        Server server = new Server();
+        server.setUrl(domain);
+
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .servers(List.of(server));
     }
 
     private Info apiInfo() {
