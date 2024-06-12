edb5101e8a9b0c3b27cb4e7a3714818d396330e9
nujinging <nujinging@naver.com>
2024-06-12 21:41:15 +0900

    [Design] 193th 댓글 추가하기 컴포넌트 수정

--- front/src/components/AddComponent.js
+++ front/src/components/AddComponent.js
@@ -1,10 +1,18 @@
-import '../scss/components/layerHeader.scss'
+import '../scss/components/addComponent.scss'
 import {memebookApi} from "../util/memebookApi";
 import {useState} from "react";
 
 export default function AddComponent(props) {
+  // 댓글 폼 활성화
+  const [textareaActive, setTextareaActive] = useState(false);
   const [memberIdx, setMemberIdx] = useState(123);
   const [contentValue, setContentValue] = useState(false);
+
+  // 댓글 클릭하면 커지기
+  const commtentActive = () => {
+    setTextareaActive(true);
+  }
+
   const contentValueCount = (event) => {
     setContentValue(event.target.value);
   }
@@ -16,7 +24,7 @@ export default function AddComponent(props) {
         wordNation : "KOR",
         memberIdx : memberIdx,
       });
-      alert('등록 완료');
+      setTextareaActive(false);
     } catch (error) {
       console.log(error)
       console.log('에러')
@@ -31,7 +39,7 @@ export default function AddComponent(props) {
   return (
     <div className="comment_input_box">
       <div className={`input_box`}>
-        <textarea className="textarea_input" placeholder="댓글 입력" name="" id="" cols="30" rows="10" maxLength={props.length} onChange={contentValueCount}></textarea>
+        <textarea placeholder="댓글 입력" className={`${textareaActive ? 'active' : ''}`} name="" id="" cols="30" rows="10" onClick={commtentActive} maxLength={props.length} onChange={contentValueCount}></textarea>
         <button type="button" className="btn_comment_submit" onClick={wordAddPost}>
           <span>등록</span>
         </button>
