7d6418b5ad57c0b4c2f66f6b2b62facb125ffb41
developyoun <pyoun820@naver.com>
2024-07-09 23:32:05 +0900

    [fix] cors 이슈 수정 테스트

--- back/src/main/java/meme/book/back/config/WebMvcConfig.java
+++ back/src/main/java/meme/book/back/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 
