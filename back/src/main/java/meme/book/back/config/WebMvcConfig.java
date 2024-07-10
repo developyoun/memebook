7815e4f6c604149fa502b555e52c067e08b5d246
pyoun <pyoun820@naver.com>
2024-07-11 02:25:03 +0900

    [conf] 이슈 수정

--- back/src/main/java/meme/book/back/config/WebMvcConfig.java
+++ back/src/main/java/meme/book/back/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
+                .allowedHeaders("*")
 //                .allowCredentials(true)
         ;
     }
