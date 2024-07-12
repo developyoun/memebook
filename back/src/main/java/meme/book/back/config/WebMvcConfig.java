8cc3ec105657a46b98f5d22744ab4d90ec420bd4
pyoun <pyoun820@naver.com>
2024-07-13 02:19:54 +0900

    [conf] 이슈 수정

--- back/src/main/java/meme/book/back/config/WebMvcConfig.java
+++ back/src/main/java/meme/book/back/config/WebMvcConfig.java
@@ -6,17 +6,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebMvcConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns("*:3000")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("*")
-////                .allowCredentials(true)
-//        ;
-//    }
-//
-//}
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*:3000")
+//                .allowCredentials(true)
+        ;
+    }
+
+}
