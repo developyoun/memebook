d08c38e6f5b5637d4bfd2d4c9068ee60ad305e53
pyoun <pyoun820@naver.com>
2024-07-13 02:25:05 +0900

    [conf] 이슈 수정

--- back/src/main/java/meme/book/back/config/WebMvcConfig.java
+++ back/src/main/java/meme/book/back/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*:3000", "memebook.co.kr/**")
+                .allowedOriginPatterns("*:3000", "https://memebook.co.kr/**")
 //                .allowCredentials(true)
         ;
     }
