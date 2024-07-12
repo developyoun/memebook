342b2c4c8ec88caaa81db0bacdc52117417156a3
pyoun <pyoun820@naver.com>
2024-07-13 02:35:18 +0900

    [conf] 이슈 수정

--- back/src/main/java/meme/book/back/config/WebMvcConfig.java
+++ back/src/main/java/meme/book/back/config/WebMvcConfig.java
@@ -14,6 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOriginPatterns("*:3000")
+                .allowedOrigins("https://memebook.co.kr")
 //                .allowCredentials(true)
         ;
     }
