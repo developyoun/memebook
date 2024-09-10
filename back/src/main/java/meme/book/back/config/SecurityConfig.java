c6275be8db77ff937deace8e3a05b59efeb7d948
pyoun <pyoun820@naver.com>
2024-09-10 23:07:04 +0900

    [dev] 소셜로그인 구현중

--- back/src/main/java/meme/book/back/config/SecurityConfig.java
+++ back/src/main/java/meme/book/back/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package meme.book.back.config;
 
+import io.netty.handler.codec.http.multipart.HttpPostRequestEncoder;
 import lombok.RequiredArgsConstructor;
 import meme.book.back.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +36,7 @@ public class SecurityConfig {
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(config -> {
                     config.requestMatchers("/docs", "/swagger-ui/**", "/v3/api-docs/**").permitAll();
+                    config.requestMatchers(HttpMethod.GET).permitAll();
                     config.requestMatchers("/auth/login").permitAll();
                     config.anyRequest().authenticated();
                 })
