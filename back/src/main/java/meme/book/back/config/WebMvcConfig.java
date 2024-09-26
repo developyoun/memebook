8cf87b0d956f373123d259ff02d430d73d00210c
pyoun <pyoun820@naver.com>
2024-09-26 22:17:12 +0900

    [dev] mvc 삭제

---
+++ back/src/main/java/meme/book/back/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package meme.book.back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*:3000", "https://memebook.co.kr")
+//                .allowCredentials(true)
+        ;
+    }
+}
